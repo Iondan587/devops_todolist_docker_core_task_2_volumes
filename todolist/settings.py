2ebfbeb82b2ee0742b29d5c15f8c45ec159c2da1
Iondan587 <danivan022008@gmail.com>
2026-07-05 10:35:23 +0300

    Solution for app 2

--- todolist/settings.py
+++ todolist/settings.py
@@ -67,8 +67,8 @@ DATABASES = {
         'NAME': 'app_db',
         'USER': 'app_user',
         'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': '172.17.0.4',
+        'PORT': '3306',
     }
 }
 
